6db2eea24159457cb2f1f501f2b4e812e3ea08ea
ShortCircuits10 <32178489+ShortCircuits10@users.noreply.github.com>
2020-01-30 09:29:55 -0500

    Added Swerve

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+  // For example to map the left and right motors, you could define the
+  // following variables to use with your drivetrain subsystem.
+  // public static int leftMotor = 1;
+  // public static int rightMotor = 2;
+
+  // If you are using multiple modules, make sure to define both the port
+  // number and the module. For example you with a rangefinder:
+  // public static int rangefinderPort = 1;
+  // public static int rangefinderModule = 1;
+  public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 2; // CAN
+  public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_ENCODER = 0; // Analog
+  public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 1; // CAN
+
+  public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 4; // CAN
+  public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_ENCODER = 1; // Analog
+  public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 3; // CAN
+
+  public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 6; // CAN
+  public static final int DRIVETRAIN_BACK_LEFT_ANGLE_ENCODER = 2; // Analog
+  public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 5; // CAN
+
+  public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 8; // CAN
+  public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_ENCODER = 3; // Analog
+  public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 7;
+
+}
---
+++ src/main/java/frc/robot/commands/SwerveDrive.java
@@ -0,0 +1,79 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.OI;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+//import edu.wpi.first.wpilibj.GenericHID;
+//import edu.wpi.first.wpilibj.drive.Vector2d;
+//import edu.wpi.first.wpilibj.Utilities;
+import org.frcteam2910.common.robot.Utilities;
+
+
+
+
+
+
+/**
+ * An example command.  You can replace me with your own command.
+ */
+public class SwerveDrive extends Command {
+  public SwerveDrive() {
+    // Use requires() here to declare subsystem dependencies
+    requires(Drivetrain.getInstance());
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    double forward = -Robot.getOi().getPrimaryJoystick().getRawAxis(1);
+    forward = Utilities.deadband(forward);
+    // Square the forward stick
+    forward = Math.copySign(Math.pow(forward, 2.0), forward);
+
+    double strafe = -Robot.getOi().getPrimaryJoystick().getRawAxis(0);
+  strafe = Utilities.deadband(strafe);
+    // Square the strafe stick
+    strafe = Math.copySign(Math.pow(strafe, 2.0), strafe);
+
+    double rotation = -Robot.getOi().getPrimaryJoystick().getRawAxis(4);
+   rotation = Utilities.deadband(rotation);
+    // Square the rotation stick
+    rotation = Math.copySign(Math.pow(rotation, 2.0), rotation);
+
+    Drivetrain.getInstance().drive(new Translation2d(forward, strafe), rotation, true);
+
+  }
+  	
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
